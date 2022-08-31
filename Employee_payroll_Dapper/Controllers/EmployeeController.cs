f2f9c4578cb5667a8432c14efc9939ae48581245
Ganeshp007 <potdart40.gp@gmail.com>
2022-08-31 17:47:27 +0530

    Ganesh/Added: created project and done required configuration

---
+++ Employee_payroll_Dapper/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_payroll_Dapper.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
